3420bdb133a45f5bc2659800d0b6a4cb575c0610
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2026-07-26 14:25:30 +0200

    Keep Modal benchmark submission CPU importable

--- src/fused_mm_sampling/modal_lib/modal_triton_benchmark.py
+++ src/fused_mm_sampling/modal_lib/modal_triton_benchmark.py
@@ -1,4 +1,3 @@
-from ..bench.triton_benchmark_lib import Args, run_triton_bechmark
 from .utils import ModalEnvConfig, make_app, make_image, make_volumes, set_volume_caches
 
 
@@ -12,14 +11,31 @@ app = make_app()
 
 
 @app.function(gpu=cfg.gpu_spec, image=make_image(), volumes=make_volumes(), timeout=cfg.timeout)
-def function(args: Args):
+def function(
+    tgt_dir: str | None,
+    case: str,
+    n_procs: int,
+    name: str | None,
+    disable_compile: bool,
+    bench_fn: str,
+):
+    from ..bench.triton_benchmark_lib import Args, run_triton_bechmark
+
     set_volume_caches()
+    args = Args(
+        tgt_dir=tgt_dir,
+        case=case,
+        n_procs=n_procs,
+        name=name,
+        disable_compile=disable_compile,
+        bench_fn=bench_fn,
+    )
     run_triton_bechmark(args)
 
 
 @app.local_entrypoint()
 def main():
-    args = Args(
+    function.remote(
         tgt_dir=cfg.tgt_dir,
         case=cfg.case,
         n_procs=cfg.n_procs,
@@ -27,4 +43,3 @@ def main():
         disable_compile=cfg.disable_compile,
         bench_fn=cfg.bench_fn,
     )
-    function.remote(args=args)
